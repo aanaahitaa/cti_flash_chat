555fc4dda32c034902210177e060655e1c7d7c52
Anahita Sabaghi <anahita.rastegar@gmail.com>
2025-01-27 18:06:36 +0330

    Set up Firebase project and integrated user registration with Firebase authentication.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,7 +1,10 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_starting_project/components/rounded_button.dart';
 import '/constants.dart';
 import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
@@ -11,6 +14,12 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var auth = FirebaseAuth.instance;
+  String errorMessage = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,47 +40,76 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             const SizedBox(
               height: 48.0,
             ),
-            TextFormField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-                labelText: 'Email',
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email',
+                      labelText: 'Email',
+                    ),
+                    controller: _emailController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (email) {
+                      return email != null && EmailValidator.validate(email)
+                          ? null
+                          : 'Please enter a valid email';
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password',
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      //Do something with the user input
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (password) {
+                      return password != null && password.length > 5
+                          ? null
+                          : 'The password should be of 6 characters at least.';
+                    },
+                  ),
+                ],
               ),
-              onChanged: (value) {
-                //Do something with the user input
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (email) {
-                return email != null && EmailValidator.validate(email)
-                    ? null
-                    : 'Please enter a valid email';
-              },
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
-                labelText: 'Password',
-              ),
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (password) {
-                return password != null && password.length > 5
-                    ? null
-                    : 'The password should be of 6 characters at least.';
-              },
             ),
             const SizedBox(
               height: 24.0,
             ),
+            Text(
+              errorMessage,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red, fontSize: 16),
+            ),
             RoundedButton(
-                color: kRegisterButtonColor,
-                title: 'Registration',
-                onPressed: () {}),
+              color: kRegisterButtonColor,
+              title: 'Registration',
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  try {
+                    auth
+                        .createUserWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: _passwordController.text)
+                        .then((value) {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    });
+                  } catch (e) {
+                    print('ERROR ${e.toString()}');
+                    setState(() {
+                      errorMessage = e.toString().split('] ')[1];
+                    });
+                  }
+                }
+              },
+            ),
             const SizedBox(height: 12),
             IconButton(
               icon: const Icon(Icons.arrow_back),
